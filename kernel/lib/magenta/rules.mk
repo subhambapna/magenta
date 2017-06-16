4d1b27899a4913a60187659d2e686cecda9c375e
Todd Eisenberger <teisenbe@google.com>
2017-06-16 15:43:39 -0700

    [magenta][iommu] Implement the IommuDispatcher
    Change-Id: I30d91eae7a96ee88c0f865b1e861d3ceb5da47f1

--- kernel/lib/magenta/rules.mk
+++ kernel/lib/magenta/rules.mk
@@ -25,6 +25,7 @@ MODULE_SRCS := \
     $(LOCAL_DIR)/handle_reaper.cpp \
     $(LOCAL_DIR)/interrupt_event_dispatcher.cpp \
     $(LOCAL_DIR)/io_mapping_dispatcher.cpp \
+    $(LOCAL_DIR)/iommu_dispatcher.cpp \
     $(LOCAL_DIR)/job_dispatcher.cpp \
     $(LOCAL_DIR)/log_dispatcher.cpp \
     $(LOCAL_DIR)/magenta.cpp \
